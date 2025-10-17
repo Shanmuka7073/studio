1c0e4e966366ea5f3f8e98a9d4fc393c39fee418
shanmuka srinivas <shanmuka7073@gmail.com>
2025-10-17 06:28:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9070259337-c267a",
+  "appId": "1:215572467483:web:88e4f0f118aa0d57b1cdbe",
+  "apiKey": "AIzaSyA6Qnsk2vz3GfABFJMM53FhNCAf74SBarE",
+  "authDomain": "studio-9070259337-c267a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "215572467483"
+};
