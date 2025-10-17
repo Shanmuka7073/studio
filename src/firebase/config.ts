b27ed8ec56e71d3c2adb420abb05e401fffca81b
shanmuka srinivas <shanmuka7073@gmail.com>
2025-10-17 17:00:47 +0000

    sorry add old one

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyAxrI0iFyVDWGVpx96k0M154g99oWbpYPc",
-  "authDomain": "clipflow-bccfj.firebaseapp.com",
-  "projectId": "clipflow-bccfj",
-  "storageBucket": "clipflow-bccfj.firebasestorage.app",
-  "messagingSenderId": "416416074412",
-  "appId": "1:416416074412:web:3b4a5eede088bbee61ad09"
+  "apiKey": "AIzaSyDs4dpNyeU1u-SY5lU0qCgGrOSeRBLqLiE",
+  "authDomain": "notification-3ae98.firebaseapp.com",
+  "projectId": "notification-3ae98",
+  "storageBucket": "notification-3ae98.appspot.com",
+  "messagingSenderId": "1044165231953",
+  "appId": "1:1044165231953:web:a17cc6638cdce72cdabf70",
+  "measurementId": "G-K83S995J6B"
 };
