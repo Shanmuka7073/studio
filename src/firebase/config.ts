aa32d656533e46f70d7a7c777cdb8e9ff8082a0f
shanmuka srinivas <shanmuka7073@gmail.com>
2025-10-18 04:57:04 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "apiKey": "AIzaSyDs4dpNyeU1u-SY5lU0qCgGrOSeRBLqLiE",
-  "authDomain": "notification-3ae98.firebaseapp.com",
-  "projectId": "notification-3ae98",
-  "storageBucket": "notification-3ae98.appspot.com",
-  "messagingSenderId": "1044165231953",
-  "appId": "1:1044165231953:web:a17cc6638cdce72cdabf70",
-  "measurementId": "G-K83S995J6B"
+  apiKey: "AIzaSyA6Qnsk2vz3GfABFJMM53FhNCAf74SBarE",
+  authDomain: "studio-9070259337-c267a.firebaseapp.com",
+  projectId: "studio-9070259337-c267a",
+  storageBucket: "studio-9070259337-c267a.appspot.com",
+  messagingSenderId: "215572467483",
+  appId: "1:215572467483:web:88e4f0f118aa0d57b1cdbe"
 };
