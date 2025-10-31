28d744088990e5c92d60ffb6ba74280aca623e22
shanmuka srinivas <shanmuka7073@gmail.com>
2025-10-31 16:46:50 +0000

    I think problem with english language which I speak, give necessary info

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -105,6 +105,7 @@ export default function CheckoutPage() {
     if (SpeechRecognition) {
         speechRecognitionRef.current = new SpeechRecognition();
         const recognition = speechRecognitionRef.current;
+        recognition.lang = 'en-IN'; // Set language to Indian English
         recognition.continuous = true;
         recognition.interimResults = true;
         
